9a336eed98937e1d0c9353fd654712ca725dc180
Diego Armando Castaño <black-lionheart@hotmail.com>
2015-03-08 21:57:09 -0500

    Finished exercise
    completed navigation functionality, added code comments and cleaned css
    styles.

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,14 @@
 
 	/**
 	*
-	* Module built to procure navigation from remote endpoint 
+	* Module built to procure navigation from remote endpoint, modules were built using the revealing module pattern 
 	*
 	**/
 	var navProcurer = (function() {
 
 		var performRequest = function(url) {
 
-			// old school ajax request, also use of native javascript promises (current specification only works on chrome and firefox)
+			// old school ajax request, also use of native javascript promises (current api only works on chrome and firefox)
 			var promise = new Promise(function(resolve, reject) {
 				var xhr = new XMLHttpRequest();
 				xhr.open("GET", url, true);
@@ -88,5 +88,121 @@
 
 	})();
 
-	
+
+	/**
+	*
+	* This module deals with user interactions upon the navigation menu and web site
+	*
+	**/
+	var navigationInteractions = (function() {
+
+		/*==========  cache most commonly queried elements  ==========*/
+
+		var menuContainer, mainContent, hamburgerContainer, overlay, logoContainer;
+
+		var cacheElements = function() {
+			menuContainer = document.querySelector('.mainNavigationContainer');
+		 	mainContent = document.querySelector('.mainContent');
+		 	hamburgerContainer = document.querySelector('.hamburgerContainer');
+		 	overlay = document.querySelector('.overlay');
+		 	logoContainer = document.querySelector('.logoContainer');
+		 	
+		}
+		
+		/**
+		*
+		* toggles the push menu
+		*
+		**/
+		var menuToggle = function(event) {
+			event.stopPropagation();
+			menuContainer.classList.toggle('pushed');
+			hamburgerContainer.classList.toggle('pressed');
+			mainContent.classList.toggle('pushed');
+			overlay.classList.toggle('showMobile');
+			logoContainer.classList.toggle('showMobile');
+		}
+
+		/**
+		*
+		* clears secondary navigation
+		*
+		**/
+		var clearSubNav = function() {
+			[].forEach.call(document.querySelectorAll('.secondaryNav'), function (el) {
+
+					 el.classList.remove('visible');
+					 el.parentNode.classList.remove('showingSubMenu');
+			});
+			document.querySelector('.overlay').classList.remove('show');
+		}
+
+		/**
+		*
+		* interaction for when main navigation is clicled
+		*
+		**/
+		var mainNavigationClicked = function(event) {
+			
+			var element = this.parentNode.querySelector('.secondaryNav'),
+				listElement = this.parentNode;
+
+			if(element !== null) {
+				//if element doesn't have secondary nav we let the default browser behavior happen and the link be followed
+				event.preventDefault();
+				event.stopPropagation();
+				clearSubNav();
+				listElement.classList.add('showingSubMenu');
+				element.classList.add('visible');
+				document.querySelector('.overlay').classList.add('show');
+
+			}
+		}
+
+		var bindEvents = function() {
+			//bind click event on main nav items
+
+			//we cache the most commonly used elements
+			cacheElements();
+
+			// attach event listeners for all main navigation items
+			[].forEach.call(document.querySelectorAll('li.topElement>a'), function (el) {
+
+				 el.addEventListener('click', mainNavigationClicked, false);
+			});
+
+			//haburger navigation event handler
+			hamburgerContainer.onclick = menuToggle;
+			//when overlay is clicked we toggle the menu
+			overlay.onclick = menuToggle;
+			
+
+			//when any element is clicked secondary navigation is cleared , handler was not attached directly because clearSubNav function is not meant to be a handler as is used in another context
+			document.onclick = function(event){
+				clearSubNav();
+			}
+			
+		}
+
+		return {
+			bindEvents: bindEvents
+		}
+
+
+	})();
+
+
+	/**
+	*
+	* kickstarts the menu craetion and interactions binding
+	*
+	**/
+	navProcurer.getNavigationData().then(function(data) {
+		var navigationMarkup = navigationBuilder.buildNavigationMarkup(data);
+		var mainNavElement = document.querySelector('.mainNavigation ul');
+		mainNavElement.innerHTML = navigationMarkup;
+		navigationInteractions.bindEvents();
+	});
+
+
 })();
